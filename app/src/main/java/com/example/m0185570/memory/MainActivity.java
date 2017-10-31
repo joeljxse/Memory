a4da4c40ed6a1a9c2c6bc5a2d598d8dff59169fe
joeljxse <joeljosewattwil@gmail.com>
2017-10-31 15:40:43 +0100

    m

---
+++ app/src/main/java/com/example/m0185570/memory/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.m0185570.memory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

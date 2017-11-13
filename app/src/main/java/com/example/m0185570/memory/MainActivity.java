9f692ab272ba0ad2c210bb403b7663353266ec18
joeljxse <joeljosewattwil@gmail.com>
2017-11-13 11:00:09 +0100

    start implementation location api

--- app/src/main/java/com/example/m0185570/memory/MainActivity.java
+++ app/src/main/java/com/example/m0185570/memory/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.m0185570.memory;
 
+import android.app.Activity;
+import android.content.Context;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
         setContentView(R.layout.activity_main);
+
+        MapView map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setMaxZoomLevel(20);
+
+        map.setMultiTouchControls(true);
+        map.setBuiltInZoomControls(true);
+
+        IMapController mapController = map.getController();
+        GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
+        mapController.setCenter(startPoint);
+
+        mapController = map.getController();
+        mapController.setZoom(18);
     }
-    //test
+
+    public void onResume(){
+        super.onResume();
+
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+    }
 }
